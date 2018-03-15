b591c5373426c4a30c1622433fac107681520477
Pooja Kamaraj <poojakkamaraj@gmail.com>
2018-03-15 22:33:34 +0530

    First

---
+++ form.php
@@ -0,0 +1,10 @@
+
+
+
+<form action="cart.php" method="post">
+<input type="hidden" name="qty" value="1">
+<input type="hidden" name="price" value="100">
+<input type="hidden" name="id" value="ab001">
+<input type="hidden" name="name" value="skirt">
+<button> <img src="order.jpg"      alt="onclick"  height="60px" width="100px" ></button>
+</form>
